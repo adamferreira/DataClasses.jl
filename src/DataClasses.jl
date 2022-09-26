08e4c8119e0b5f2971056c53c0a100b6601520f2
adamferreira <adam.ferreira.dc@gmail.com>
2022-09-26 19:22:21 +0000

    Implementation and test of quickdataclass and dataclass

--- src/DataClasses.jl
+++ src/DataClasses.jl
@@ -1,13 +1,64 @@
 module DataClasses
-
-export AbstractDataClass
+import Base
 
 abstract type AbstractDataClass end
 
-function from_dict(type::Type{T}, d::Dict) where T <: AbstractDataClass
-    for (attrname, attrtype) in zip(fieldnames(type), fieldtypes(type))
-        println((attrname, attrtype))
+# Macro to define an AbstractDataClass subtype with a single line
+macro quickdataclass(T, stmts...)
+    # Only support attribute declaration expression in quickdataclass macro
+    for stmt in stmts
+        @assert isa(stmt, Expr)
+        @assert stmt.head == Symbol("::")
+    end
+    # Escapes declaration so field::type does not become
+    # field::DataClasses.<type> at macro resolution
+    clean_statements = [esc(stmt) for stmt in stmts]
+    return quote
+        mutable struct $(esc(T)) <: AbstractDataClass
+            # Struct parameters
+            $(clean_statements...)
+            # Incomplete constructor pattern
+            $(T)() = new()
+        end
     end
 end
+
+# Macro to define an AbstractDataClass subtype with a block
+macro dataclass(T, block)
+    # Make sure the macro is given a block as input
+    @assert block.head == :block
+    # Escapes block content as it should not be resolved as belonging to DataClasses module
+    # Otherwise every user types in the block will becone DataClasses.<type>
+    clean_statements = [esc(stmt) for stmt in block.args]
+    return quote
+        mutable struct $(esc(T)) <: AbstractDataClass
+            # Block content
+            $(clean_statements...)
+            # Incomplete constructor pattern
+            $(T)() = new()
+        end
+    end
+end
+
+function from_dict(type::Type{T}, d::Dict) where T <: AbstractDataClass
+    # First create an instance of type T
+    # TODO : Error if T ahas no empty constructor
+    dataclass::T = T()
+    for (attr::Symbol, attrtype::DataType) in zip(fieldnames(type), fieldtypes(type))
+        if haskey(d, String(attr))
+            setfield!(dataclass, attr, Base.convert(attrtype, d[String(attr)]))
+        end
+    end
+    #setfield!(dataclass, Symbol(attrname), convert(symbols[Symbol(attrname)], attrval))
+    return dataclass
+end
+
+
+# Cast overload
+Base.convert(::Type{T}, x::Dict) where T <: AbstractDataClass = from_dict(T, x)
+
+export AbstractDataClass
+export @dataclass, @quickdataclass
+export from_dict
 
 end
--- test/from_dict.jl
+++ test/from_dict.jl
@@ -1,8 +1,108 @@
 using DataClasses
+using Test
 
-mutable struct TestDataClass <: DataClasses.AbstractDataClass
-    field1::Int
-    field2
+@testset "From Dict Basic" begin
+    mutable struct TestDataClass <: AbstractDataClass
+        field1::Int
+        field2::Float64
+    
+        # Incomplete constructor pattern
+        TestDataClass() = new()
+    end
+
+    data = DataClasses.from_dict(TestDataClass, Dict("field1" => 5, "field2" => 3.14))
+    @test data.field1 == 5
+    @test data.field2 == 3.14
 end
 
-DataClasses.from_dict(TestDataClass, Dict("field1" => 5))
+@testset "Quick Data Class" begin
+    @quickdataclass TestDataClass field1::Int field2::Float64
+    data = DataClasses.from_dict(TestDataClass, Dict("field1" => 5, "field2" => 3.14))
+    @test data.field1 == 5
+    @test data.field2 == 3.14
+end
+
+@testset "Data Class" begin
+    @dataclass TestDataClass begin
+        field1::Int
+        field2::Float64
+    end
+
+    data = DataClasses.from_dict(TestDataClass, Dict("field1" => 5, "field2" => 3.14))
+    @test data.field1 == 5
+    @test data.field2 == 3.14
+end
+
+@testset "Using convert from dict to Data Class" begin
+    @quickdataclass TestDataClass field1::Int field2::Float64
+    data = convert(TestDataClass, Dict("field1" => 5, "field2" => 3.14))
+    @test data.field1 == 5
+    @test data.field2 == 3.14
+end
+
+@testset "From Dict Default fiels" begin
+    mutable struct TestDataClassDefault <: AbstractDataClass
+        field1::Int
+        field2::Float64
+        field3::String
+        TestDataClassDefault() = new(0, 3.14, "")
+    end
+
+    data = DataClasses.from_dict(TestDataClassDefault, Dict("field1" => 5, "field3" => "toto"))
+    @test data.field1 == 5
+    @test data.field2 == 3.14
+    @test data.field3 == "toto"
+end
+
+@testset "Nested DataClasses" begin
+    mutable struct TestDataClassInner <: AbstractDataClass
+        subfield1::Vector{Int64}
+        subfield2::Tuple{Int, Int}
+        TestDataClassInner() = new()
+    end
+    mutable struct TestDataClassNested <: AbstractDataClass
+        field1::Int
+        field2::Float64
+        field3::TestDataClassInner
+        TestDataClassNested() = new()
+    end
+
+    d = Dict(
+        "field1" => 5, 
+        "field2" => 3.14,
+        "field3" => Dict(
+            "subfield1" => [1,2,3,4],
+            "subfield2" => (1,2)
+        )
+    )
+
+    data = DataClasses.from_dict(TestDataClassNested, d)
+    @test data.field1 == 5
+    @test data.field2 == 3.14
+    @test data.field3.subfield1 == [1,2,3,4]
+    @test data.field3.subfield2 == (1,2)
+end
+
+@testset "Nested DataClasses from macro" begin
+    @quickdataclass TestDataClassInner subfield1::Vector{Int64} subfield2::Tuple{Int, Int}
+    @dataclass TestDataClassNested begin
+        field1::Int
+        field2::Float64
+        field3::TestDataClassInner
+    end
+
+    d = Dict(
+        "field1" => 5, 
+        "field2" => 3.14,
+        "field3" => Dict(
+            "subfield1" => [1,2,3,4],
+            "subfield2" => (1,2)
+        )
+    )
+
+    data = DataClasses.from_dict(TestDataClassNested, d)
+    @test data.field1 == 5
+    @test data.field2 == 3.14
+    @test data.field3.subfield1 == [1,2,3,4]
+    @test data.field3.subfield2 == (1,2)
+end
